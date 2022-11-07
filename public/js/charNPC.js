069a0d22c186cf2ee3688a985948de4d190c7183
nsoutherland <nsoutherland@pugetsound.edu>
2022-11-07 02:01:27 -0800

    Began creating properties for npc and character objects that will be replaced/filled in with the data submitted to and stored on the server

--- public/js/charNPC.js
+++ public/js/charNPC.js
@@ -1,9 +1,19 @@
 //Create object for storing NPC data
-let npc={};
+let npc={
+    name: "", class: "",  level:1, background: "", playername: "", 
+    race: "", alignment: "", experience:0
+   
+
+
+};
 
 
 
 //Create object for storing character sheet info
-let character={};
+let character={
+    name: "",  class: "",  level:1, background: "", playername: "", 
+    race: "", alignment: "", experience:0
+
+};
 
 
