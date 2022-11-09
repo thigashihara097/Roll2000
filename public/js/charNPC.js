6ad91d7fd7ea8b515f995956d94aa7de5d670ee4
nsoutherland <nsoutherland@pugetsound.edu>
2022-11-09 03:57:45 -0800

    Added placeholder  base stats in an array and explained which one is which

--- public/js/charNPC.js
+++ public/js/charNPC.js
@@ -1,8 +1,11 @@
 //Create object for storing NPC data
 let npc={
-    name: "", class: "",  level:1, background: "", playername: "", 
-    race: "", alignment: "", experience:0
-   
+    name: "Melody", class: "bard",  level:1, background: "musician", playername: "See Sharp", 
+    race: "Human", alignment: "Chaotic good", experience:0, 
+   stats:[0, 0, 0, 0, 0, 0]
+   /*Strength is stats[0], dexterity is stats[1], constitution is stats[2], 
+   intelligence is stats[3], wisdom is stats[4], and charisma is stats[5].
+*/
 
 
 };
@@ -11,8 +14,12 @@ let npc={
 
 //Create object for storing character sheet info
 let character={
-    name: "",  class: "",  level:1, background: "", playername: "", 
-    race: "", alignment: "", experience:0
+    name: "Gandalf",  class: "wizard",  level:1, background: "Maair", playername: "Gandalf", 
+    race: "Aasimar", alignment: "Lawful good", experience:0,
+    stats:[0, 0, 0, 0, 0, 0]
+    /*Strength is stats[0], dexterity is stats[1], constitution is stats[2], 
+    intelligence is stats[3], wisdom is stats[4], and charisma is stats[5].
+ */
 
 };
 
