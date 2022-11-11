2c4a462c0c0c181691449cee9efab99eb8c7cd34
nsoutherland <nsoutherland@pugetsound.edu>
2022-11-11 02:41:56 -0800

    Added more stats and features to NPC and character sheet objects

--- public/js/charNPC.js
+++ public/js/charNPC.js
@@ -1,11 +1,26 @@
 //Create object for storing NPC data
 let npc={
-    name: "Melody", class: "bard",  level:1, background: "musician", playername: "See Sharp", 
+    name: "Melody", class: "bard",  level:1, background: "musician", dmname: "See Sharp", 
     race: "Human", alignment: "Chaotic good", experience:0, 
-   stats:[0, 0, 0, 0, 0, 0]
+   stats:[0, 0, 0, 0, 0, 0],
    /*Strength is stats[0], dexterity is stats[1], constitution is stats[2], 
-   intelligence is stats[3], wisdom is stats[4], and charisma is stats[5].
+   intelligence is stats[3], wisdom is stats[4], charisma is stats[5].
 */
+    statMods:[0, 0, 0, 0, 0, 0], 
+      /*Strength modifier is statmods[0], dexterity modifier is statmods[1], 
+      constitution modifier is statmods[2], intelligence modifier is statmods[3], 
+      wisdom modifier is statmods[4], charisma is statmods[5].
+*/
+    moreStats:[0, 0, 0],
+    /* moreStats[0] is perception, moreStats[1] is inspiration and moreStats[2] is 
+    proficiency bonus. 
+    */
+    saveThrows:[0, 0, 0, 0, 0, 0]
+/* saveThrows[0] is strength saving throw, saveThrows[1] is dexterity saving throw, 
+saveThrows[2] is constitution saving throw, saveThrows[3] is intelligence saving throw, 
+saveThrows[4] is wisdom saving throw and saveThrows[5] is charisma saving throw.
+*/
+
 
 
 };
@@ -14,12 +29,29 @@ let npc={
 
 //Create object for storing character sheet info
 let character={
-    name: "Gandalf",  class: "wizard",  level:1, background: "Maair", playername: "Gandalf", 
+    name: "Gandalf",  class: "wizard",  level:1, background: "Maair", playername: "Eru", 
     race: "Aasimar", alignment: "Lawful good", experience:0,
-    stats:[0, 0, 0, 0, 0, 0]
-    /*Strength is stats[0], dexterity is stats[1], constitution is stats[2], 
-    intelligence is stats[3], wisdom is stats[4], and charisma is stats[5].
- */
+    stats:[0, 0, 0, 0, 0, 0],
+   /*Strength is stats[0], dexterity is stats[1], constitution is stats[2], 
+   intelligence is stats[3], wisdom is stats[4], charisma is stats[5].
+*/
+    statMods:[0, 0, 0, 0, 0, 0], 
+     /*Strength modifier is statmods[0], dexterity modifier is statmods[1], 
+      constitution modifier is statmods[2], intelligence modifier is statmods[3], 
+      wisdom modifier is statmods[4], charisma is statmods[5].
+*/
+moreStats:[0, 0, 0],
+/* moreStats[0] is perception, moreStats[1] is inspiration and moreStats[2] is 
+proficiency bonus. 
+*/
+saveThrows:[0, 0, 0, 0, 0, 0]
+/* saveThrows[0] is strength saving throw, saveThrows[1] is dexterity saving throw, 
+saveThrows[2] is constitution saving throw, saveThrows[3] is intelligence saving throw, 
+saveThrows[4] is wisdom saving throw and saveThrows[5] is charisma saving throw.
+*/
+
+
+
 
 };
 
