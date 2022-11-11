136d2bd9f3855fb712ca46061d0d864cd1596d29
bappitybapcat <elizabethposy@gmail.com>
2022-11-11 09:28:31 -0800

    Adds integration with html inputs for board creation. Adds tile class.

--- public/js/board.js
+++ public/js/board.js
@@ -1,10 +1,42 @@
 // Board object
 let board = {
+    // Variables
     width: 0,
     height: 0,
+    tiles: [],
     
-    // Set width method
-    setWidth: function() {
-        width = 1;
+    // Initialization of new board
+    // Board coords are (width, height)
+    initializeBoard: function() {
+        if(Number.isNaN(width) || Number.isNaN(height)) {
+             throw console.error("Nan detected");
+        }
+        for(let i = 0; i < w; i++) {
+            tiles[i] = [];
+            for(let j = 0; j < h; i++) {
+                console.log("hello (" + i + ", " + j + ")");
+                tiles[i][j] = "(" + i + ", " + j + ")";
+                
+            }
+        }
+        console.log(tiles)
     }
 };
+
+
+// Tile object
+let tile = {
+    // Variables
+    wall: false,
+    contents: 0,
+
+    // Make wall method
+    setWall: function() {
+        wall = true;
+    },
+
+    // Change tile contents method
+    changeContents: function() {
+        contents = "a friend";
+    }
+}
