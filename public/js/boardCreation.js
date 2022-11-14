1202e8ad71e92f0b630b408df07b61b6b66f879a
bappitybapcat <elizabethposy@gmail.com>
2022-11-14 13:06:30 -0800

    Combined board.js with boardCreation.js file

--- public/js/boardCreation.js
+++ public/js/boardCreation.js
@@ -1,7 +1,48 @@
-// Import board and tile classes
-import("board.js");
-
 addEventListener("DOMContentLoaded", main);
+
+
+// Board object
+let board = {
+    // Variables
+    width: 0,
+    height: 0,
+    tiles: [],
+    
+    // Initialization of new board
+    // Board coords are (width, height)
+    initializeBoard: function() {
+        if(Number.isNaN(width) || Number.isNaN(height)) {
+             throw console.error("NaN detected");
+        }
+        for(let i = 0; i < w; i++) {
+            tiles[i] = [];
+            for(let j = 0; j < h; i++) {
+                console.log("hello (" + i + ", " + j + ")");
+                tiles[i][j] = "(" + i + ", " + j + ")";
+                
+            }
+        }
+        console.log(tiles)
+    }
+};
+
+
+// Tile object
+let tile = {
+    // Variables
+    wall: false,
+    contents: 0,
+
+    // Make wall method
+    setWall: function() {
+        wall = true;
+    },
+
+    // Change tile contents method
+    changeContents: function() {
+        contents = "a friend";
+    }
+}
 
 function main() {
     console.log("loaded");
@@ -11,7 +52,7 @@ function main() {
     // Create a new board
     function createBoard() {
         console.log("click");
-        let b = new board();
+        let b = new Object();
         b.width = document.querySelector("#width").value;
         b.height = document.querySelector("#height").value;
         b.initializeBoard();
