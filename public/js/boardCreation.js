79a03f1c18ac94bede83f53637805f929a2eaac0
bappitybapcat <elizabethposy@gmail.com>
2022-11-21 12:48:48 -0800

    Adds blank table of the specified size to the board page by modifying the DOM

--- public/js/boardCreation.js
+++ public/js/boardCreation.js
@@ -1,13 +1,11 @@
 addEventListener("DOMContentLoaded", main);
 
 function main() {
-    console.log("loaded");
     // Submit button listener
     document.querySelector("button").addEventListener("click", createBoard);
 
     // Create a new board
     function createBoard() {
-        console.log("click");
         let w = document.querySelector("#width").value;
         let h = document.querySelector("#height").value;
         let b = new Board(w, h);
@@ -16,9 +14,6 @@ function main() {
 
     // Board object
     function Board(w, h) {
-        console.log("Initializing...");
-        console.log("width: " + w + ", height: " + h);
-
         if(Number.isNaN(w) || Number.isNaN(h)) {
             throw console.error("NaN detected");
         }
@@ -28,32 +23,54 @@ function main() {
         this.height = h;
         this.tiles = [];
 
+        // Create a blank table to display the board later
+        addTable();
+
+        // Populate tiles array
         for(let i = 0; i < w; i++) {
-            console.log("i: " + i)
             this.tiles[i] = [];
+            addRow();
             for(let j = 0; j < h; j++) {
-                console.log("hello (" + i + ", " + j + ")");
                 this.tiles[i][j] = "(" + i + ", " + j + ")";
-                
+                addCell(i);
             }
         }
         console.log(this.tiles)
     }
 
+    // DOM manipulation function - Add a table to the page
+    function addTable() {
+        let t = document.createElement("table");
+        document.body.appendChild(t);
+    }
+
+    // DOM manipulation function - Add rows to the table
+    function addRow() {
+        let t = document.querySelector("table");
+        let r = document.createElement("tr");
+        t.appendChild(r);
+    } 
+
+    // DOM manipulation function - Add cells to the table
+    function addCell(row) {
+        let t = document.querySelectorAll("tr")[row];
+        let c = document.createElement("td");
+        t.appendChild(c);
+    }
 
     // Tile object
-    let tile = {
+    function Tile() {
         // Variables
-        wall: false,
-        contents: 0,
+        this.wall = false;
+        this.contents = 0;
 
         // Make wall method
-        setWall: function() {
+        function setWall() {
             wall = true;
-        },
+        }
 
         // Change tile contents method
-        changeContents: function() {
+        function changeContents() {
             contents = "a friend";
         }
     }
