68e8fcb1ee7880d109ccdbf0bbc4fec3526a0acc
nsoutherland <nsoutherland@pugetsound.edu>
2022-12-02 11:35:33 -0800

    Added more parts of the character sheet, simplified code and eliminated irrelevant info

--- public/js/charNPC.js
+++ public/js/charNPC.js
@@ -8,7 +8,7 @@ submit.addEventListener("click", getInfo);
 
 class Character{
 constructor(name, Class, level, dmname, race, alignment, experince, proficiency,stats, modArray, perception, 
-inspiration){
+inspiration, aC, hp, movSpeed, initiative){
 this.name=name; this.Class=Class; this.level=level; this.dmname=dmname; 
 this.race=race; this.alignment=alignment; this.experience=experince;
 this.proficiency=proficiency;
@@ -23,18 +23,19 @@ this.proficiency=5;
 }else if(this.level>=17){
 this.proficiency=6;    
 }
-this.stats=stats;
-this.modArray=modArray;
-this.perception=perception;
-this.inspiration=inspiration;
+this.stats=stats; this.modArray=modArray;
+this.perception=perception; this.inspiration=inspiration; this.aC=aC; 
+this.hp=hp;
+this.movSpeed=movSpeed;
+this.initiative=initiative;
 }
 }
 
 class Npc extends Character{
 constructor(name, Class, level, dmname, race, alignment, experince, proficiency, stats, modArray, perception, 
-inspiration){
+inspiration, aC, hp, movSpeed, initiative){
 super(name, Class, level, dmname, race, alignment, experince, proficiency, stats, modArray, perception, 
-inspiration); 
+inspiration, aC, hp, movSpeed, initiative); 
 }
 }
 
@@ -56,11 +57,6 @@ saveThrows[4] is wisdom saving throw and saveThrows[5] is charisma saving throw.
 */
 Skills:[],
 profLang:[], 
-movSpeed:20,
-aC:10,
-initiative:1,
-hP:20, 
-//hitDice: roll 1d10
 deathSavesSuccesses:0,
 deathSavesFails:0,
 spellCastingClass:"",
@@ -83,11 +79,6 @@ saveThrows[4] is wisdom saving throw and saveThrows[5] is charisma saving throw.
 */
 Skills:[],
 profLang:[], 
-movSpeed:30,
-aC:23,
-initiative:1,
-hP:30, 
-//hitDice: roll 1d10
 deathSavesSuccesses:0,
 deathSavesFails:0,
 spellCastingClass:"",
@@ -115,9 +106,12 @@ function getInfo(){
      let wis=document.getElementById("wis");
      let cha=document.getElementById("cha");
      let ins=document.getElementById("ins");
-   // let arc=document.getElementById("armor");
-   // let speed=document.getElementById("speed");
-  // let hitPoints=document.getElementById("hp");
+    let arc=document.getElementById("armor");
+    let speed=document.getElementById("speed");
+    let hitPoints=document.getElementById("hp");
+    const aC=arc.value;
+    const hp=hitPoints.value;
+    const movSpeed=speed.value;
     const stats=[]; 
     stats.push(str.value, dex.value,con.value, int.value, wis.value, cha.value); 
     const mods=[]; 
@@ -129,7 +123,6 @@ function getInfo(){
          mods.push(Math.round(calc));
     }
     const perception=10+mods[4];
-    if (h.value=="Character"){
     const name=a.value;
     const Class=b.value;
     const level=c.value;
@@ -139,20 +132,14 @@ function getInfo(){
     const experience=g.value;
     const inspiration=ins.value;
     let proficiency=0;
+    const initiative=mods[1];
+    if (h.value=="Character"){
     const char=new Character(name, Class, level, playername, race, alignment, experience, proficiency, stats, mods, perception, 
-    inspiration); 
+    inspiration, aC, hp, movSpeed, initiative); 
     return char; 
     }else if (h.value=="NPC"){
-    const name=a.value;
-    const Class=b.value;
-    const level=c.value;
-    const playername=d.value;
-    const race=e.value;
-    const alignment=f.value;
-    const experience=g.value;
-    let proficiency=0;
     const npc=new Npc(name, Class, level, playername, race, alignment, experience, proficiency, stats, mods, perception, 
-    inspiration); 
+    inspiration, aC, hp, movSpeed, initiative); 
     return npc;    
     }
 }
@@ -160,20 +147,8 @@ function getInfo(){
 //Actually stores the info
 function characterInfo(){
     if(h.value=="Character"){
-         
     character.profLang.push("Common");
-       
-        character.aC=arc.value;
-        character.initiative=Math.round(dMod);
-        character.movSpeed=speed.value;
-        character.hP=hitPoints.value;
     }else if(h.value=="NPC"){
-       
         npc.profLang.push("Common");
-        
-        npc.aC=arc.value;
-        npc.initiative=Math.round(dMod);
-        npc.movSpeed=speed.value;
-        npc.hP=hitPoints.value;
     }
 }
