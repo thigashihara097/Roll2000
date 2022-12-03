4a4ed51ad6e3fffe33d12f5f23898e6ff3372768
bappitybapcat <elizabethposy@gmail.com>
2022-12-03 00:38:31 -0800

    Added ability to move characters back to the character list on the sidebar

--- public/js/boardCreation.js
+++ public/js/boardCreation.js
@@ -1,3 +1,10 @@
+/* List of things to do:
+    Add a list of characters on the board (in the backend)
+    Be able to take the characters off the board
+    When you right-click on a cell it pulls up a pop up
+
+*/
+
 addEventListener("DOMContentLoaded", main);
 
 function main() {
@@ -7,10 +14,13 @@ function main() {
 
     // Create a new board
     function createBoard() {
+        // Check if there is already a table and remove if so
         if(document.querySelector("table") != null) {
             let t = document.querySelector("table");
             t.parentNode.removeChild(t);
         }
+
+        // Get the input values and create a board
         let w = document.querySelector("#width").value;
         let h = document.querySelector("#height").value;
         let b = new Board(w, h);
@@ -27,10 +37,26 @@ function main() {
     *   @param array of character objects 
     */
     function displayCharacters(characters) {
+        // Check if this is the first time, if not remove previous list, if so set up event listeners for the list div
+        let pre = document.querySelector("main").querySelectorAll("div")[1].querySelectorAll("p");
+        if(pre.length != 0) {
+            for(let p of pre) {
+                p.parentNode.removeChild(p);
+            }
+        } else {
+            let d = document.querySelector("#character");
+            console.log(d)
+            d.addEventListener("dragover", dragOver);
+            d.addEventListener("drop", drop);
+        }
+
+        // Go through the passed in array and create a p for each
         for(let character of characters) {
             let c = document.createElement("p");
             document.querySelector("main").querySelectorAll("div")[1].appendChild(c);
             c.textContent = character;
+
+            // Make each character moveable
             c.setAttribute("draggable", "true");
             c.addEventListener("dragstart", dragStart);
             c.addEventListener("dragend", dragEnd);
@@ -104,12 +130,14 @@ function main() {
     // When a drag starts
     function dragStart(event) {
         event.dataTransfer.setData("text/html", event.target.innerHTML);
+        console.log("start")
     }
 
     // Preparing table cells for dropping
     function dragOver(event) {
         event.preventDefault();
         event.dataTransfer.dropEffect = "move";
+        console.log("over")
     }
 
     // Actually dropping
@@ -119,6 +147,13 @@ function main() {
         let p = document.createElement("p");
         event.target.appendChild(p);
         p.innerHTML = character;
+
+        // Make it movable itself
+        p.setAttribute("draggable", "true");
+        p.addEventListener("dragstart", dragStart);
+        p.addEventListener("dragend", dragEnd);
+
+        console.log("boop")
     }
 
     // After a drop is complete
@@ -126,5 +161,6 @@ function main() {
         if(event.dataTransfer.dropEffect !== "none") {
             event.target.remove();
         }
+        console.log("done")
     }
 }
